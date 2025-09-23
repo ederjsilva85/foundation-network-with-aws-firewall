d254c76b0da57a4a514ab3ce5d8133355a700169
Eder de Jesus Silva <74740984+ederjsilva85@users.noreply.github.com>
2025-09-23 18:19:42 -0300

    Update inspection-network-firewall.tf

--- inspection-network-firewall.tf
+++ inspection-network-firewall.tf
@@ -1,5 +1,5 @@
 resource "aws_networkfirewall_firewall" "inspection_firewall_noprod" {
-  name                = "claroti-aws-firewall-us-east-1-inspection-noprod"
+  name                = "aws-firewall-us-east-1-inspection-noprod"
   firewall_policy_arn = aws_networkfirewall_firewall_policy.inspection_firewall_policy_noprod.arn
   vpc_id              = aws_vpc.vpc_inspection_noprod.id
  
@@ -13,7 +13,7 @@ resource "aws_networkfirewall_firewall" "inspection_firewall_noprod" {
 }
  
 resource "aws_networkfirewall_firewall_policy" "inspection_firewall_policy_noprod" {
-  name = "claroti-aws-fw-policy-us-east-1-inspection-noprod"
+  name = "aws-fw-policy-us-east-1-inspection-noprod"
  
   firewall_policy {
     stateless_default_actions          = ["aws:forward_to_sfe"]
