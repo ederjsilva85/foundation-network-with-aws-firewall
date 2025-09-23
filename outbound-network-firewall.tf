0c969d09ec40bec7e395b02ed987928886ca9cb5
Eder de Jesus Silva <74740984+ederjsilva85@users.noreply.github.com>
2025-09-23 18:26:33 -0300

    Update outbound-network-firewall.tf

--- outbound-network-firewall.tf
+++ outbound-network-firewall.tf
@@ -1,5 +1,5 @@
 resource "aws_networkfirewall_firewall" "outbound_firewall_noprod" {
-  name                = "claroti-aws-firewall-us-east-1-outbound-noprod"
+  name                = "aws-firewall-us-east-1-outbound-noprod"
   firewall_policy_arn = aws_networkfirewall_firewall_policy.outbound_firewall_policy_noprod.arn
   vpc_id              = aws_vpc.vpc_outbound_noprod.id
  
@@ -13,7 +13,7 @@ resource "aws_networkfirewall_firewall" "outbound_firewall_noprod" {
 }
  
 resource "aws_networkfirewall_firewall_policy" "outbound_firewall_policy_noprod" {
-  name = "claroti-aws-fw-policy-us-east-1-outbound-noprod"
+  name = "aws-fw-policy-us-east-1-outbound-noprod"
  
   firewall_policy {
     stateless_default_actions          = ["aws:forward_to_sfe"]
